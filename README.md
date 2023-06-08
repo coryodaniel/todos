ba1ce7f3d7bf83731095975fb51f69e38d1f5b91
Cory O'Daniel <cory@coryodaniel.com>
2023-06-08 09:21:37 -0700

    create todo

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Golang TODO Server
 
 This is companion code for my GFY talk.
+
+It's not a pinnacle of good golang code, it's a kitchen sink.
--- cmd/client.go
+++ cmd/client.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"flag"
 	"fmt"
+	"strings"
 
 	"github.com/coryodaniel/todo/pkg/sdk"
 )
@@ -35,7 +36,7 @@ func (c *ClientCommand) Init(args []string) error {
 func (c *ClientCommand) Run() error {
 	todoClient := sdk.NewClient(c.url)
 
-	switch subcommand := c.fs.Args()[0]; subcommand {
+	switch subcommand := c.fs.Arg(0); subcommand {
 	case "list":
 		todos, _ := todoClient.ListTodos()
 		fmt.Println("Here are your todos:")
@@ -45,7 +46,10 @@ func (c *ClientCommand) Run() error {
 		}
 
 	case "create":
-		fmt.Println("TODO")
+		title := strings.Join(c.fs.Args()[1:], " ")
+		todo, _ := todoClient.CreateTodo(title)
+
+		fmt.Printf("Added todo: %s [%s]\n", todo.Title, todo.ID)
 	default:
 		return fmt.Errorf("unknown subcommand: %s", subcommand)
 	}
